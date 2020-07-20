b598b3cc518501f7682cb3bd242902c193b4c71d
Ali <atavakoli@cellular.de>
2020-07-20 11:35:12 +0200

    Initial Commit

---
+++ Examole CellularModelDataSource/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examole CellularModelDataSource
+//
+//  Created by Ali Tavakoli on 20.07.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
